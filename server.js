a8e3629b2d67f6a83c8d0bdb51738e9ef970de39
leocabs29 <152485105+leocabs29@users.noreply.github.com>
2025-10-29 21:02:24 +0800

    test

--- server.js
+++ server.js
@@ -7,23 +7,16 @@ import expenseRoutes from "./routes/expenseRoutes.js";
 dotenv.config();
 
 const app = express();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
-
-// Routes
 app.use("/api/expenses", expenseRoutes);
 
-// Test route
 app.get("/", (req, res) => {
   res.send("✅ Backend is running!");
 });
 
-// Connect MongoDB
 const connectDB = async () => {
   try {
-    console.log("Connecting to MongoDB...");
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: "ShessentialsDB",
     });
@@ -36,6 +29,9 @@ const connectDB = async () => {
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
 
+// Required for Vercel
 export default app;
